564a23eef8985e0f42eb9b6a5ccd5536ed29d316
Hudson Luiz Sales Schumaker <hudson.schumaker@me.com>
2021-04-14 09:02:35 +0200

    Update scroll.c

--- src/scrolling/scroll.c
+++ src/scrolling/scroll.c
@@ -40,7 +40,7 @@ void main(void) {
   pal_col(0, 0x0D);	  // set screen to black
   pal_col(1, 0x02);	  // blue
   pal_col(2, 0x02);	  // blue
-  pal_col(3, 0x21);	// cyan?
+  pal_col(3, 0x21);	  // cyan?
 
   // write text to name table
   put_str(NTADR_A(2, 0), "SchumakerTeam");
